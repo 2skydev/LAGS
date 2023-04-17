4ce9b41b428641334ac4e1f369d1f11762c7de91
2sky <2skydev@gmail.com>
2023-04-17 19:18:36 +0900

    높은 호환성을 위해 탭 업데이트시 트리거 하는 방식으로 변경

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 const normalTabDetectKey = '&lost_ark_sign_normal_tab=1'
 
-chrome.tabs.onCreated.addListener(tab => {
-  const url = tab.pendingUrl || '';
-
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  const { status, url } = changeInfo
+  
   if (!url) return;
+  if (status !== 'loading') return;
   if (url.indexOf('https://accounts.google.com') !== 0) return;
   if (!url.includes('onstove.com')) return;
   if (url.includes(normalTabDetectKey)) return;
 
   chrome.windows.create({
     incognito: false,
-    url: tab.pendingUrl + normalTabDetectKey
+    url: url + normalTabDetectKey
   })
 
   chrome.tabs.remove(tab.id)
