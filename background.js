aa0e8c3aaa863520cc7b1199e1179daadaf240a3
2sky <2skydev@gmail.com>
2023-04-18 16:37:15 +0900

    호환성 처리

--- background.js
+++ background.js
@@ -1,18 +1,24 @@
 const normalTabDetectKey = '&lost_ark_sign_normal_tab=1'
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const { status, url } = changeInfo
-  
-  if (!url) return;
-  if (status !== 'loading') return;
-  if (url.indexOf('https://accounts.google.com') !== 0) return;
-  if (!url.includes('onstove.com')) return;
-  if (url.includes(normalTabDetectKey)) return;
+const processUrl = (url, tabId) => {
+  if (!url) return
+  if (url.indexOf('https://accounts.google.com') !== 0) return
+  if (!url.includes('onstove.com')) return
+  if (url.includes(normalTabDetectKey)) return
 
   chrome.windows.create({
     incognito: false,
     url: url + normalTabDetectKey
   })
 
-  chrome.tabs.remove(tab.id)
+  chrome.tabs.remove(tabId)
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== 'loading') return
+  processUrl(changeInfo.url, tab.id)
+})
+
+chrome.tabs.onCreated.addListener((tab) => {
+  processUrl(tab.pendingUrl || '', tab.id)
 })
